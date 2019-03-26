a46865551292d384f3240adcca7f5b5cf4f19348
Sharon <srcbun@gmail.com>
2019-03-25 22:42:31 -0400

    remove test folder

--- corgi/js testing/functions/forEach.js
+++
@@ -1,9 +0,0 @@
-var colors = ["red", "orange", "yellow", "green", "blue", "indigo"];
-for (var i=0; i<=colors.length; i++){
-  console.log(colors[i]);
-}
-
-// foreach vs for
-colors.forEach(function(i){
-  console.log(i);
-});
